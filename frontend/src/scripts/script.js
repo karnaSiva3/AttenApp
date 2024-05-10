7da9d05a6c296df539e389f2f9af34d5e332507a
AdarshRai3 <adarshaibusiness@gmail.com>
2024-05-11 00:08:49 +0530

    Completed Responsivenss of Sidebar for mobile

--- frontend/src/scripts/script.js
+++ frontend/src/scripts/script.js
@@ -1,8 +1,16 @@
 let sidebar = document.querySelector(".sidebar");
 let sidebarBtn = document.querySelector(".sidebarBtn");
+let btn = document.querySelector(".btn");
 sidebarBtn.onclick = function () {
   sidebar.classList.toggle("active");
   if (sidebar.classList.contains("active")) {
     sidebarBtn.classList.replace("bx-menu", "bx-menu-alt-right");
   } else sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+};
+
+btn.onclick = function () {
+  sidebar.classList.toggle("active");
+  if (sidebar.classList.contains("active")) {
+    btn.classList.replace("bx-menu", "bx-menu-alt-right");
+  } else btn.classList.replace("bx-menu-alt-right", "bx-menu");
 };
